ce9cb8fc2c687a28f2163c9b9acde2d8a30257da
Gage Lieble <gagelieble@hotmail.com>
2023-02-06 15:47:39 -0800

    setting DRF

--- user_app/urls.py
+++ user_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'user_app'
 
 urlpatterns = [
-    path('', views.landing, name='landing')
+    path('', views.landing, name='landing'),
+    path('api/', views.UserView.as_view())
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
 # Create your views here.
 
 def landing(request):
     return render(request, 'user/index.html')
+
+class UserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
