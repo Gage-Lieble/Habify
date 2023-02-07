42264a4b6aa76f46ea34da53df13db0c3478ae4b
Gage Lieble <gagelieble@hotmail.com>
2023-02-07 10:57:19 -0800

    Successfully passed CSRF token from django to react

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,10 +1,12 @@
 import SignUpForm from "./components/forms/SignUpForm";
+import LoginForm from "./components/forms/LoginForm";
 
 function App() {
   return (
     <div className="App">
      <h2>REACT USER</h2>
-     <SignUpForm/>
+     <SignUpForm/><br />
+     <LoginForm />
     </div>
   );
 }
